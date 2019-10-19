03631e454a32334379b026b1c4edeb779350d04e
Vedant Patwari <vedantpatwari01@gmail.com>
2019-10-19 02:39:29 -0500

    Random Fixes + Test Scene Modifications
    --

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     public PressurePlate myP;
+
     // Start is called before the first frame update
     void Start()
     {
         myP.Pressure_Plate_Activated += OpenDoor;
         myP.Pressure_Plate_Deactivated += CloseDoor;
-    }
+        GameObject.FindObjectOfType<Lever>().LeverTogglingAction += OpenDoor;
+}
 
     // Update is called once per frame
     void Update()
