73f720ad2619789abc33b0f48f9b38f8fd0eb29f
Vedant Patwari <vedantpatwari01@gmail.com>
2019-10-22 15:52:15 -0500

    Created Prefabs + Started Map Scripts
     - created prefabs for drone, door, lever, pressure plate, and crate
     - started working on basic map/level unlock scripts

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public MapObject[] MapsInLevels;
+    public float RangeToShow = 5f;
+    public Drone myDrone;
+
+    private MapObject closest;
+    private int numberOfUnlockedLevels = 1; 
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("unlockedLevels"))
+        {
+            numberOfUnlockedLevels = PlayerPrefs.GetInt("unlockedLevels");
+        }
+        
+
+        updateLevels();
+        InvokeRepeating("refreshClosestMap", 0.05f, 0.25f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) { 
+            
+        }
+    }
+
+    private void DisplayMapCard() {
+        if (Vector3.Distance(closest.transform.position, myDrone.transform.position) < RangeToShow) {
+            closest.showInfoCard();
+        }
+    }
+
+    public void updateLevels() {
+        for (int i = 0; i < numberOfUnlockedLevels; i++) {
+            MapsInLevels[i].unlockMap();
+        } 
+    }
+
+    private void refreshClosestMap() {
+        float minDist = 0;
+        foreach (MapObject m in MapsInLevels) {
+            float dist = Vector3.SqrMagnitude(m.transform.position - myDrone.transform.position);
+            if (minDist == 0 || dist < minDist) {
+                minDist = dist;
+                closest = m;
+            }
+        }
+    }
+
+    public float GetMinimumDistance() {
+        return RangeToShow;
+    }
+}
---
+++ Assets/Scripts/MapObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapObject : MonoBehaviour
+{
+    public GameObject infoCard;
+
+    private MapManager myMap;
+    private Drone myDrone; 
+    private bool unlocked = false;
+    private bool isShowing = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMap = GameObject.FindObjectOfType<MapManager>();
+        myDrone = GameObject.FindObjectOfType<Drone>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isShowing) {
+            if (Vector3.Distance(myDrone.transform.position, this.transform.position) > myMap.GetMinimumDistance()) {
+                hideInfoCard();
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0) && unlocked) { 
+            // play level 
+        }
+    }
+
+    public void unlockMap() {
+        unlocked = true;
+    }
+
+    public void showInfoCard() {
+        isShowing = true;
+        infoCard.SetActive(true);
+    }
+
+    private void hideInfoCard() {
+        isShowing = false;
+        infoCard.SetActive(false);
+    }
+
+
+}
