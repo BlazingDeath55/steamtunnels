67e0f0376d6d40563f2e03f7757759af54a9e6e7
Corey Tolbert <ctolbert555@gmail.com>
2019-11-24 11:34:03 -0600

    Added Steam and Pipes
    Added Steam and Pipes

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -10,23 +10,31 @@ public class Lever : MonoBehaviour
     public float thresholdDistance = 4f;
     int which;
 
-    private Drone myD;
+    private Drone[] myDs;
 
     // Start is called before the first frame update
     void Start()
     {
 
-        myD = GameObject.FindObjectOfType<Drone>();
-        myD.ButtonPressed += OnButton;
+        myDs = GameObject.FindObjectsOfType<Drone>();
+        foreach(Drone myD in myDs)
+        {
+            myD.ButtonPressed += OnButton;
+        }
         which = 0;
     }
 
     void OnButton() {
-        Vector3 PosVec = this.gameObject.transform.position - myD.transform.position;
-        if (Vector3.SqrMagnitude(PosVec) <= thresholdDistance + Mathf.Pow(PosVec.z, 2)) {
-            Debug.Log("Togelled Lever");
-            ToggleSprites();
-            LeverTogglingAction();           
+        foreach (Drone myD in myDs)
+        {
+            Vector3 PosVec = this.gameObject.transform.position - myD.transform.position;
+            //ONLY ACTIVATES ONCE, CANNOT BE UN-PULLED
+            if (Vector3.SqrMagnitude(PosVec) <= thresholdDistance + Mathf.Pow(PosVec.z, 2) && which == 0)
+            {
+                Debug.Log("Togelled Lever");
+                ToggleSprites();
+                LeverTogglingAction();
+            }
         }
     }
 
