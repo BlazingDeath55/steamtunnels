943f82dfbba6054d2cbdbc4fb0d85d5281366a78
Corey Tolbert <ctolbert555@gmail.com>
2019-11-24 23:17:24 -0600

    Added a bunch of updates, graphically and otehrwise
    Added a bunch of updates, graphically and otehrwise (darkness)

---
+++ Assets/Scripts/SteamSys.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteamSys : MonoBehaviour
+{
+
+    public Valve myV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myV.SteamOff += ToggleOff;
+        myV.SteamOn += ToggleOn;
+    }
+
+    void ToggleOff()
+    {
+        Debug.Log("Steam OFF");
+        transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+    void ToggleOn()
+    {
+        Debug.Log("Steam ON");
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+}
---
+++ Assets/Scripts/Valve.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Valve : MonoBehaviour
+{
+    public UnityAction SteamOff, SteamOn;
+    public float thresholdDistance = 4f;
+    int which;
+
+    private Drone[] myDs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myDs = GameObject.FindObjectsOfType<Drone>();
+        foreach (Drone myD in myDs)
+        {
+            myD.ButtonPressed += OnButton;
+        }
+        which = 0;
+    }
+
+    void OnButton()
+    {
+        foreach (Drone myD in myDs)
+        {
+            Vector3 PosVec = this.gameObject.transform.position - myD.transform.position;
+            //ONLY ACTIVATES ONCE, CANNOT BE UN-PULLED
+            if (Vector3.SqrMagnitude(PosVec) <= thresholdDistance + Mathf.Pow(PosVec.z, 2))
+            {
+                Debug.Log("Togelled Lever");
+                ToggleSprites();
+                if (which == 0)
+                {
+                    SteamOff();
+                }
+                else
+                {
+                    SteamOn();
+                }
+            }
+        }
+    }
+
+    void ToggleSprites()
+    {
+        transform.GetChild(which).gameObject.SetActive(false);
+        which++;
+        which %= 2;
+        transform.GetChild(which).gameObject.SetActive(true);
+    }
+}
