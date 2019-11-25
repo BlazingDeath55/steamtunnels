9db672db9e398bd2b9e4f4bef4c39b00b67e3393
Corey Tolbert <ctolbert555@gmail.com>
2019-11-25 11:34:14 -0600

    All Levels fully functional
    All Levels fully functional

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -10,32 +10,25 @@ public class Lever : MonoBehaviour
     public float thresholdDistance = 4f;
     int which;
 
-    private Drone[] myDs;
+    public Drone myD;
 
     // Start is called before the first frame update
     void Start()
     {
-
-        myDs = GameObject.FindObjectsOfType<Drone>();
-        foreach(Drone myD in myDs)
-        {
-            myD.ButtonPressed += OnButton;
-        }
+        myD.ButtonPressed += OnButton;
         which = 0;
     }
 
     void OnButton() {
-        foreach (Drone myD in myDs)
+        Vector3 PosVec = this.gameObject.transform.position - myD.transform.position;
+        //ONLY ACTIVATES ONCE, CANNOT BE UN-PULLED
+        if (Vector3.SqrMagnitude(PosVec) <= thresholdDistance + Mathf.Pow(PosVec.z, 2) && which == 0)
         {
-            Vector3 PosVec = this.gameObject.transform.position - myD.transform.position;
-            //ONLY ACTIVATES ONCE, CANNOT BE UN-PULLED
-            if (Vector3.SqrMagnitude(PosVec) <= thresholdDistance + Mathf.Pow(PosVec.z, 2) && which == 0)
-            {
-                Debug.Log("Togelled Lever");
-                ToggleSprites();
-                LeverTogglingAction();
-            }
+            Debug.Log("Togelled Lever");
+            ToggleSprites();
+            LeverTogglingAction();
         }
+
     }
 
     void ToggleSprites()
--- Assets/Scripts/Valve.cs
+++ Assets/Scripts/Valve.cs
@@ -9,38 +9,31 @@ public class Valve : MonoBehaviour
     public float thresholdDistance = 4f;
     int which;
 
-    private Drone[] myDs;
+    public Drone myD;
 
     // Start is called before the first frame update
     void Start()
     {
-        myDs = GameObject.FindObjectsOfType<Drone>();
-        foreach (Drone myD in myDs)
-        {
-            myD.ButtonPressed += OnButton;
-        }
+        myD.ButtonPressed += OnButton;
         which = 0;
     }
 
     void OnButton()
     {
-        foreach (Drone myD in myDs)
+        Vector3 PosVec = this.gameObject.transform.position - myD.transform.position;
+        //ONLY ACTIVATES ONCE, CANNOT BE UN-PULLED
+        if (Vector3.SqrMagnitude(PosVec) <= thresholdDistance + Mathf.Pow(PosVec.z, 2))
         {
-            Vector3 PosVec = this.gameObject.transform.position - myD.transform.position;
-            //ONLY ACTIVATES ONCE, CANNOT BE UN-PULLED
-            if (Vector3.SqrMagnitude(PosVec) <= thresholdDistance + Mathf.Pow(PosVec.z, 2))
+            Debug.Log("Togelled Lever");  
+            if (which == 0)
             {
-                Debug.Log("Togelled Lever");
-                ToggleSprites();
-                if (which == 0)
-                {
-                    SteamOff();
-                }
-                else
-                {
-                    SteamOn();
-                }
+                SteamOff();
             }
+            else
+            {
+                SteamOn();
+            }
+            ToggleSprites();
         }
     }
 
