d66752698a95b0631f6a8fc0b56607873cfb73d9
Corey Tolbert <ctolbert555@gmail.com>
2019-11-25 12:26:41 -0600

    Added LevelSelector Scenes
    Added LevelSelector Scenes

---
+++ Assets/Scripts/PasswordCheck.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PasswordCheck : MonoBehaviour
+{
+
+    public string correctPassword;
+    public int whichLvlLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckPass(string s)
+    {
+        if(s==correctPassword)
+        {
+            SceneManager.LoadScene(whichLvlLoad);
+        }
+        else
+        {
+            //INCORRECT
+            Debug.Log("WRONG");
+        }
+    }
+}
