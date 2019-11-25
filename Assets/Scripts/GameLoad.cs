2d06341c9d1a17cba1340c103b7246b7c8fb06c4
Corey Tolbert <ctolbert555@gmail.com>
2019-11-25 16:18:16 -0600

    Setup Build Settings
    Setup Build Settings

---
+++ Assets/Scripts/GameLoad.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLoad : MonoBehaviour
+{
+    int scene_num;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scene_num = 1;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Pickup1") > 0.01)
+        {
+            SceneManager.LoadScene(scene_num);
+        }
+    }
+}
